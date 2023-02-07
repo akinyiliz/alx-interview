bfb7613b56503faacc692fd4142fc938de2a1cb9
akinyiliz <lizzieke84@gmail.com>
2023-02-07 22:41:48 +0300

    completed task 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def minOperations(n):
+    if n <= 0:
+        return 0
+
+    operations = 0
+    i = 2
+    while i <= n:
+        operations += 1
+        if i * 2 > n:
+            break
+        i *= 2
+
+    return operations + minOperations(n - i)
