e93860f33f13357c57cb54462aa1cf068ddad8b2
akinyiliz <lizzieke84@gmail.com>
2023-02-07 23:04:07 +0300

    completed task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,15 +3,42 @@
 
 
 def minOperations(n):
-    if n <= 0:
+    '''Calculates the fewest number of operations needed to result in exactly n H characters in the file.
+    Returns an integer: If n is impossible to achieve, return 0'''
+    pasted = 1
+    clipboard = 0
+    counter = 0
+
+    while pasted < n:
+        # did not copy anything yet
+        if clipboard == 0:
+            clipboard = pasted
+            counter += 1
+
+        # haven't pasted anything yet
+        if pasted == 1:
+            pasted += clipboard
+            counter +=1
+            continue
+
+        # remaining chars to paste
+        remaining = n - pasted
+
+        if remaining < clipboard:
+            return 0
+
+        if remaining % pasted != 0:
+            # paste current clipboard
+            pasted += clipboard
+            counter += 1
+        else:
+            # copy all
+            clipboard = pasted
+            pasted += clipboard
+            counter += 2
+
+    # if desired result is gotten
+    if pasted == n:
+        return counter
+    else:
         return 0
-
-    operations = 0
-    i = 2
-    while i <= n:
-        operations += 1
-        if i * 2 > n:
-            break
-        i *= 2
-
-    return operations + minOperations(n - i)
