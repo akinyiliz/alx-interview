adf5aa5dc570941735dfcdfbb54b35ccf17bc2a8
akinyiliz <lizzieke84@gmail.com>
2023-03-16 21:33:28 +0300

    Fixed pycodestyle error

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -21,4 +21,4 @@ def rotate_2d_matrix(matrix):
         for j in range(int(n / 2)):
             temp = matrix[i][j]
             matrix[i][j] = matrix[i][n - 1 - j]
-            matrix[i][n - 1 - j] =  temp
+            matrix[i][n - 1 - j] = temp
